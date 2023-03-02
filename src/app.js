fb38366254bd3249351df51b7a38164b2fb6b33d
Phoebeziregbe <ziregbephoebe28@gmail.com>
2023-03-02 12:31:26 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -72,57 +72,67 @@ function farTemperature(event) {
   let fahrenheiTemp = (celsiusElement * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheiTemp);
+}
 
-  function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.remove("active");
-    farLink.classList.add("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusElement);
-  }
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  farLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusElement);
+}
 
-  function formatDay(timestamp) {
-    let fordate = new Date(timestamp * 1000);
-    let day = date.getDay;
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return days[day];
-  }
+  return days[day];
+}
 
-  function displayForecast(response) {
-    let forecastElement = document.querySelector(`#forecast`);
-    forecast = response.data.daily;
-    let forecastHTML = `<div class =row>`;
-    forecast.forEach(function (forecastDay, index) {
-      if (index < 6) {
-        forecastHTML =
-          forecastHTML +
-          `<div class="col-2" >
-      <div class="weak">${formatDay(forecastDay.dt)}</div>;
-      <img src="http://openweathermap.org/img/wn/${forecastDay}.weather[0].icon}@2x.png"
-      alt=""
-      width="42"/>
+function displayForecast(response) {
+  let forecast = response.data.daily;
 
+  let forecastElement = document.querySelector("#forecast");
 
-    <div class="weather-forecast-temp">
-      <span class="weather-forecast-max">${Math.round(
-        forecastDay.temp.max
-      )} °</span>
-      <span class="weather-forecast-min">${Math.round(
-        forecastDay.temp.min
-      )} °</span>
-    </div>`;
-      }
-    });
-  }
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-
-  let farLink = document.querySelector("#far-link");
-  farLink.addEventListener("click", farTemperature);
-
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-  let form = document.querySelector("#search-input");
-  form.addEventListener("submit", search);
 }
+
+let farLink = document.querySelector("#far-link");
+farLink.addEventListener("click", farTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+let form = document.querySelector("#search-input");
+form.addEventListener("submit", search);
+
+theHandle("Paris");
